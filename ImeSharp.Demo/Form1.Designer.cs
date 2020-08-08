7bfb1487fd1e6964689654635bf49e3ac656e161
ryancheung <ryancheung.go@gmail.com>
2020-08-08 17:39:52 +0800

    Add demo project

---
+++ ImeSharp.Demo/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ImeSharp.Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelComp = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxCandidates = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Press F1 to toggle IME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 297);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "InputResult:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CompositionString:";
+            // 
+            // labelComp
+            // 
+            this.labelComp.AutoSize = true;
+            this.labelComp.Location = new System.Drawing.Point(134, 99);
+            this.labelComp.Name = "labelComp";
+            this.labelComp.Size = new System.Drawing.Size(0, 12);
+            this.labelComp.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "CandidateList";
+            // 
+            // textBoxCandidates
+            // 
+            this.textBoxCandidates.Enabled = false;
+            this.textBoxCandidates.Location = new System.Drawing.Point(16, 140);
+            this.textBoxCandidates.Multiline = true;
+            this.textBoxCandidates.Name = "textBoxCandidates";
+            this.textBoxCandidates.Size = new System.Drawing.Size(345, 139);
+            this.textBoxCandidates.TabIndex = 6;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Enabled = false;
+            this.textBoxResult.Location = new System.Drawing.Point(97, 294);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(264, 21);
+            this.textBoxResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxCandidates);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelComp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelComp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxCandidates;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ ImeSharp.Demo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImeSharp.Demo
+{
+    public partial class Form1 : Form
+    {
+        private bool _inputMethodEnabled;
+        public Form1()
+        {
+            InitializeComponent();
+
+            CenterToScreen();
+
+            Application.Idle += Application_Idle;
+            KeyDown += Form1_KeyDown;
+
+            InputMethod.WindowHandle = Handle;
+        }
+
+        private void Application_Idle(object sender, EventArgs e)
+        {
+            FakeDraw();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                InputMethod.EnableOrDisableInputMethod(!_inputMethodEnabled);
+
+                _inputMethodEnabled = !_inputMethodEnabled;
+            }
+        }
+
+        private void FakeDraw()
+        {
+        }
+
+    }
+}
