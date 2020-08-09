862666545d6d04d948b77005b212035d1d039ffe
ryancheung <ryancheung.go@gmail.com>
2020-08-09 14:00:21 +0800

    Add lots of native constants and WndProc hook handling methods

--- ImeSharp/Native/NativeMethods.cs
+++ ImeSharp/Native/NativeMethods.cs
@@ -5,8 +5,33 @@ namespace ImeSharp.Native
 {
     public partial class NativeMethods
     {
+        #region Constants
+
         public const int S_OK = 0x00000000;
         public const int S_FALSE = 0x00000001;
+
+        public const int WM_KEYFIRST = 0x0100;
+        public const int WM_KEYDOWN = 0x0100;
+        public const int WM_KEYUP = 0x0101;
+        public const int WM_CHAR = 0x0102;
+        public const int WM_DEADCHAR = 0x0103;
+        public const int WM_SYSKEYDOWN = 0x0104;
+        public const int WM_SYSKEYUP = 0x0105;
+        public const int WM_SYSCHAR = 0x0106;
+        public const int WM_SYSDEADCHAR = 0x0107;
+        public const int WM_UNICHAR = 0x0109;
+        public const int WM_KEYLAST = 0x0109;
+        public const int UNICODE_NOCHAR = 0xFFFF;
+
+        public const int WM_NOTIFY = 0x004E;
+        public const int WM_INPUTLANGCHANGEREQUEST = 0x0050;
+        public const int WM_INPUTLANGCHANGE = 0x0051;
+        public const int WM_TCARD = 0x0052;
+        public const int WM_HELP = 0x0053;
+        public const int WM_USERCHANGED = 0x0054;
+        public const int WM_NOTIFYFORMAT = 0x0055;
+
+        #endregion Constants
 
         [DllImport("user32.dll")]
         public static extern int GetSystemMetrics(SM nIndex);
@@ -20,5 +45,13 @@ namespace ImeSharp.Native
 
         [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
         public static extern IntPtr GetKeyboardLayout(int dwLayout);
+
+        public const int GWL_WNDPROC = -4;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
     }
 }
--- ImeSharp/Native/NativeMethodsIMM32.cs
+++ ImeSharp/Native/NativeMethodsIMM32.cs
@@ -5,6 +5,68 @@ namespace ImeSharp.Native
 {
     public partial class NativeMethods
     {
+        #region Constants
+
+        public const int WM_IME_SETCONTEXT = 0x0281;
+        public const int WM_IME_NOTIFY = 0x0282;
+        public const int WM_IME_CONTROL = 0x0283;
+        public const int WM_IME_COMPOSITIONFULL = 0x0284;
+        public const int WM_IME_SELECT = 0x0285;
+        public const int WM_IME_CHAR = 0x0286;
+        public const int WM_IME_REQUEST = 0x0288;
+        public const int WM_IME_KEYDOWN = 0x0290;
+        public const int WM_IME_KEYUP = 0x0291;
+        public const int WM_IME_STARTCOMPOSITION = 0x010D;
+        public const int WM_IME_ENDCOMPOSITION = 0x010E;
+        public const int WM_IME_COMPOSITION = 0x010F;
+        public const int WM_IME_KEYLAST = 0x010F;
+
+        // wParam of report message WM_IME_NOTIFY
+        public const int IMN_CLOSESTATUSWINDOW = 0x0001;
+        public const int IMN_OPENSTATUSWINDOW = 0x0002;
+        public const int IMN_CHANGECANDIDATE = 0x0003;
+        public const int IMN_CLOSECANDIDATE = 0x0004;
+        public const int IMN_OPENCANDIDATE = 0x0005;
+        public const int IMN_SETCONVERSIONMODE = 0x0006;
+        public const int IMN_SETSENTENCEMODE = 0x0007;
+        public const int IMN_SETOPENSTATUS = 0x0008;
+        public const int IMN_SETCANDIDATEPOS = 0x0009;
+        public const int IMN_SETCOMPOSITIONFONT = 0x000A;
+        public const int IMN_SETCOMPOSITIONWINDOW = 0x000B;
+        public const int IMN_SETSTATUSWINDOWPOS = 0x000C;
+        public const int IMN_GUIDELINE = 0x000D;
+        public const int IMN_PRIVATE = 0x000E;
+
+        // wParam of report message WM_IME_REQUEST
+        public const int IMR_COMPOSITIONWINDOW = 0x0001;
+        public const int IMR_CANDIDATEWINDOW = 0x0002;
+        public const int IMR_COMPOSITIONFONT = 0x0003;
+        public const int IMR_RECONVERTSTRING = 0x0004;
+        public const int IMR_CONFIRMRECONVERTSTRING = 0x0005;
+        public const int IMR_QUERYCHARPOSITION = 0x0006;
+        public const int IMR_DOCUMENTFEED = 0x0007;
+
+        // parameter of ImmGetCompositionString
+        public const int GCS_COMPREADSTR = 0x0001;
+        public const int GCS_COMPREADATTR = 0x0002;
+        public const int GCS_COMPREADCLAUSE = 0x0004;
+        public const int GCS_COMPSTR = 0x0008;
+        public const int GCS_COMPATTR = 0x0010;
+        public const int GCS_COMPCLAUSE = 0x0020;
+        public const int GCS_CURSORPOS = 0x0080;
+        public const int GCS_DELTASTART = 0x0100;
+        public const int GCS_RESULTREADSTR = 0x0200;
+        public const int GCS_RESULTREADCLAUSE = 0x0400;
+        public const int GCS_RESULTSTR = 0x0800;
+        public const int GCS_RESULTCLAUSE = 0x1000;
+
+        public const int GCS_COMP = (GCS_COMPSTR | GCS_COMPATTR | GCS_COMPCLAUSE);
+        public const int GCS_COMPREAD = (GCS_COMPREADSTR | GCS_COMPREADATTR | GCS_COMPREADCLAUSE);
+        public const int GCS_RESULT = (GCS_RESULTSTR | GCS_RESULTCLAUSE);
+        public const int GCS_RESULTREAD = (GCS_RESULTREADSTR | GCS_RESULTREADCLAUSE);
+
+        #endregion Constants
+
         [DllImport("imm32.dll", SetLastError = true)]
         public static extern IntPtr ImmCreateContext();
 
