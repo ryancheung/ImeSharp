6b40323d0262e39eb9bb712912985543853f2361
ryancheung <ryancheung.go@gmail.com>
2020-08-19 09:24:21 +0800

    Update demo

--- ImeSharp.Demo/Form1.cs
+++ ImeSharp.Demo/Form1.cs
@@ -13,6 +13,42 @@ namespace ImeSharp.Demo
     public partial class Form1 : Form
     {
         private string _inputContent = string.Empty;
+
+        private void OnTextInput(char character)
+        {
+            switch (character)
+            {
+                case '\b':
+                    if (_inputContent.Length > 0)
+                        _inputContent = _inputContent.Remove(_inputContent.Length - 1, 1);
+                    break;
+                case '\r':
+                    _inputContent = "";
+                    break;
+                default:
+                    _inputContent += character;
+                    break;
+            }
+
+            textBoxResult.Text = _inputContent;
+        }
+
+        private void OnTextComposition(IMEString compositionText, int cursorPosition, IMEString[] candidateList, int candidatePageStart, int candidatePageSize, int candidateSelection)
+        {
+            var str = compositionText.ToString();
+            str = str.Insert(cursorPosition, "|");
+            labelComp.Text = str;
+
+            string candidateText = string.Empty;
+
+            for (int i = 0; candidateList != null && i < candidateList.Length; i++)
+                candidateText += string.Format("  {2}{0}.{1}\r\n", i + 1, candidateList[i], i == candidateSelection ? "*" : "");
+
+            textBoxCandidates.Text = candidateText;
+
+            InputMethod.SetTextInputRect(labelComp.Location.X + labelComp.Size.Width, labelComp.Location.Y, 0, labelComp.Size.Height);
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -23,41 +59,18 @@ namespace ImeSharp.Demo
             KeyDown += Form1_KeyDown;
 
             InputMethod.Initialize(this.Handle, false);
+            InputMethod.TextInputCallback = OnTextInput;
+            InputMethod.TextCompositionCallback = OnTextComposition;
 
-            InputMethod.TextInput += (s, e) =>
-            {
-                switch (e.Character)
-                {
-                    case '\b':
-                        if (_inputContent.Length > 0)
-                            _inputContent = _inputContent.Remove(_inputContent.Length - 1, 1);
-                        break;
-                    case '\r':
-                        _inputContent = "";
-                        break;
-                    default:
-                        _inputContent += e.Character;
-                        break;
-                }
+            //InputMethod.TextInput += (s, e) =>
+            //{
+            //    OnTextInput(e.Character);
+            //};
 
-                textBoxResult.Text = _inputContent;
-            };
-
-            InputMethod.TextComposition += (s, e) =>
-            {
-                var str = e.CompositionString.ToString();
-                str = str.Insert(e.CursorPosition, "|");
-                labelComp.Text = str;
-
-                string candidateList = string.Empty;
-
-                for (int i = 0; e.CandidateList != null && i < e.CandidateList.Length; i++)
-                    candidateList += string.Format("  {2}{0}.{1}\r\n", i + 1, e.CandidateList[i], i == e.CandidateSelection ? "*" : "");
-
-                textBoxCandidates.Text = candidateList;
-
-                InputMethod.SetTextInputRect(labelComp.Location.X + labelComp.Size.Width, labelComp.Location.Y, 0, labelComp.Size.Height);
-            };
+            //InputMethod.TextComposition += (s, e) =>
+            //{
+            //    OnTextComposition(e.CompositionText, e.CursorPosition, e.CandidateList, e.CandidatePageStart, e.CandidatePageSize, e.CandidateSelection);
+            //};
         }
 
         private void Application_Idle(object sender, EventArgs e)
