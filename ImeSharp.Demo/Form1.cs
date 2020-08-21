f37c0992f9beec5a9bed47b21db52d39eaa88fb9
ryancheung <ryancheung.go@gmail.com>
2020-08-21 15:45:59 +0800

    Update Demo, mimic MonoGame game looping

--- ImeSharp.Demo/Form1.cs
+++ ImeSharp.Demo/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ImeSharp.Native;
+using NativeMessage = ImeSharp.Native.NativeMethods.MSG;
 
 namespace ImeSharp.Demo
 {
@@ -74,9 +78,32 @@ namespace ImeSharp.Demo
             //};
         }
 
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        private static extern bool PeekMessage(out NativeMessage msg, IntPtr hWnd, uint messageFilterMin, uint messageFilterMax, uint flags);
+
+        // Mimic MonoGame game loop
         private void Application_Idle(object sender, EventArgs e)
         {
-            FakeDraw();
+            var msg = new NativeMessage();
+            do
+            {
+                FakeDraw();
+            }
+            while (!PeekMessage(out msg, IntPtr.Zero, 0, 0, 0) && IsDisposed == false);
+        }
+
+        // Mimic MonoGame WndProc
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case NativeMethods.WM_KEYDOWN:
+                    //var now = DateTime.Now;
+                    //Debug.WriteLine("{0}:{1}.{2} - Form1.WndProc.WM_KEYDOWN {3}", now.ToShortTimeString(), now.Second, now.Millisecond, m.WParam);
+                    break;
+            }
+
+            base.WndProc(ref m);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
