372ed83ae314e1e6eedc392541eb8f741555651e
ryancheung <ryancheung.go@gmail.com>
2024-03-13 15:36:06 +0800

    Add .net 7 target back; enables aot publish for demo project

--- ImeSharp.Demo/Program.cs
+++ ImeSharp.Demo/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +15,8 @@ namespace ImeSharp.Demo
         [STAThread]
         static void Main()
         {
+            ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
+
             //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
